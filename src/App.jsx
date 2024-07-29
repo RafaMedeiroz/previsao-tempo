736c8223e6c8d13fae5073ee99bed3af11026a54
Rafael Medeiros <rafaelmedeiros.rm@outlook.com>
2024-07-29 10:55:16 -0300

    inicio-projeto

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useRef } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const inputRef = useRef()
+
+  function searchCity(){
+    console.log(inputRef.current.value)
+
+  }
+
+  return (
+    <div>
+      <h1>Previsão do tempo</h1>
+      <input ref={inputRef} type='text' placeholder='Digite o nome da cidade'/>
+      <button onClick={searchCity}>Buscar</button>
+    </div>
+  )
+}
+
+export default App
